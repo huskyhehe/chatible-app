4ea5f42329ab0c011a31eef6c6033164b7e3fdac
huskyhehe <zhou.he1@northeastern.edu>
2023-01-15 17:41:22 -0800

    Add celery delay to call tasks

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,10 +3,13 @@ import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
+from chat import tasks
+
 
 class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
+        tasks.get_response.delay(self.channel_name, text_data_json)
 
         # The consumer ChatConsumer is synchronous while the channel layer
         # methods are asynchronous. Therefore, wrap the methods in async-to-sync
