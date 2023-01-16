f4eb8d39296c9fd431b817a1a7cb75c6cbe69bbe
huskyhehe <zhou.he1@northeastern.edu>
2023-01-15 17:41:05 -0800

    Add celery tasks

---
+++ chat/tasks.py
@@ -0,0 +1,25 @@
+from asgiref.sync import async_to_sync
+from celery import shared_task
+from channels.layers import get_channel_layer
+from chatterbot import ChatBot
+from chatterbot.ext.django_chatterbot import settings
+
+channel_layer = get_channel_layer()
+
+
+@shared_task
+def get_response(channel_name, input_data):
+    chatbot = ChatBot(**settings.CHATTERBOT)
+    response = chatbot.get_response(input_data)
+    response_data = response.serialize()
+    print(response_data)
+
+    async_to_sync(channel_layer.send)(
+        channel_name,
+        {
+            "type": "chat.message",
+            "text": {"msg": response_data["text"], "source": "bot"},
+        },
+    )
+
+
